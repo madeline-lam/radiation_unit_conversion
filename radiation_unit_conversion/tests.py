a8123d00e0129dae69a466a775cf761dd6511210
Evert Nasedkin <nasedkinevert@gmail.com>
2024-12-12 00:18:58 +0000

    flake8

--- radiation_unit_conversion/tests.py
+++ radiation_unit_conversion/tests.py
@@ -1,23 +1,25 @@
 import unittest
 import numpy as np
-import astropy.constants as cst
 import astropy.units as u
 import units as units
 
-test_flux = np.linspace(1,10,10)
-test_wavelength = np.linspace(1,5,10)
+test_flux = np.linspace(1, 10, 10)
+test_wavelength = np.linspace(1, 5, 10)
 
 class TestUnitsConversions(unittest.TestCase):
+
 
     def setUp(self):
         """Set up test data"""
         self.test_flux = np.linspace(1, 10, 10)  # Array of flux values from 1 to 10 W/m^2
+
 
     def test_watt_metersquared2erg_cmsquared_second(self):
         """Test conversion from W/m^2 to erg/cm^2/s"""
         expected_output = self.test_flux * 1000  # erg/cm^2/s
         result = units.watt_metersquared2erg_cmsquared_second(self.test_flux)
         np.testing.assert_allclose(result, expected_output, rtol=1e-6)
+
 
     def test_erg_cmsquared_second2watt_metersquared(self):
         """Test conversion from erg/cm^2/s to W/m^2"""
@@ -26,17 +28,20 @@ class TestUnitsConversions(unittest.TestCase):
         result = units.erg_cmsquared_second2watt_metersquared(test_flux_erg)
         np.testing.assert_allclose(result, expected_output, rtol=1e-6)
 
+
     def test_round_trip_conversion(self):
         """Test that a round-trip conversion returns the original values."""
         intermediate_flux = units.watt_metersquared2erg_cmsquared_second(self.test_flux)
         final_flux = units.erg_cmsquared_second2watt_metersquared(intermediate_flux)
         np.testing.assert_allclose(self.test_flux, final_flux, rtol=1e-6)
 
+
     def test_watt_metersquared_hertz2erg_cmsquared_second_hertz(self):
         """Test conversion from W/m^2/Hz to erg/cm^2/s/Hz"""
         expected_output = self.test_flux * 1000
         result = units.watt_metersquared_hertz2erg_cmsquared_second_hertz(self.test_flux)
         np.testing.assert_allclose(result, expected_output, rtol=1e-6)
+
 
     def test_erg_cmsquared_secondhertz2watt_metersquaredhertz(self):
         """Test conversion from erg/cm^2/s/Hz to W/m^2/Hz"""
@@ -45,12 +50,14 @@ class TestUnitsConversions(unittest.TestCase):
         result = units.erg_cmsquared_secondhertz2watt_metersquaredhertz(test_flux_erg)
         np.testing.assert_allclose(result, expected_output, rtol=1e-6)
 
+
     def test_watt_metersquared_hertz2erg_cmsquared_second_angstrom(self):
         """Test conversion from W/m^2/Hz to erg/cm^2/s/\u00c5ngström"""
         constant = 2.99792458e21
         expected_output = constant * self.test_flux / self.test_wavelength**2
         result = units.watt_metersquared_hertz2erg_cmsquared_second_angstrom(self.test_flux, self.test_wavelength)
         np.testing.assert_allclose(result, expected_output, rtol=1e-6)
+
 
     def test_erg_cmsquared_second_angstrom2watt_metersquared_hertz(self):
         """Test conversion from erg/cm^2/s/\u00c5ngström to W/m^2/Hz"""
@@ -60,12 +67,14 @@ class TestUnitsConversions(unittest.TestCase):
         result = units.erg_cmsquared_second_angstrom2watt_metersquared_hertz(test_flux_erg, self.test_wavelength)
         np.testing.assert_allclose(result, expected_output, rtol=1e-6)
 
+
     def test_watt_metersquared_micron2watt_metersquared_hertz(self):
         """Test conversion from W/m^2/\u03bcm to W/m^2/Hz"""
         constant = 2.99792458e14
         expected_output = constant * self.test_flux / self.test_wavelength**2
         result = units.watt_metersquared_micron2watt_metersquared_hertz(self.test_flux, self.test_wavelength)
         np.testing.assert_allclose(result, expected_output, rtol=1e-6)
+
 
     def test_watt_metersquared_hertz2watt_metersquared_micron(self):
         """Test conversion from W/m^2/Hz to W/m^2/\u03bcm"""
@@ -75,12 +84,14 @@ class TestUnitsConversions(unittest.TestCase):
         result = units.watt_metersquared_hertz2watt_metersquared_micron(test_flux_hz, self.test_wavelength)
         np.testing.assert_allclose(result, expected_output, rtol=1e-6)
 
+
     def test_photon_cmsquared_second_micron2watt_metersquared_micron(self):
         """Test conversion from photon/cm^2/s/μm to W/m^2/μm"""
         constant = 5.03411250e14
         expected_output = self.test_flux / self.test_wavelength / constant
         result = units.photon_cmsquared_second_micron2watt_metersquared_micron(self.test_flux, self.test_wavelength)
         np.testing.assert_allclose(result, expected_output, rtol=1e-6)
+
 
     def test_erg_cmsquared_second_angstrom2photon_cmsquared_second_angstrom(self):
         """Test conversion from erg/cm^2/s/Ångström to photon/cm^2/s/Ångström"""
@@ -89,6 +100,7 @@ class TestUnitsConversions(unittest.TestCase):
         result = units.erg_cmsquared_second_angstrom2photon_cmsquared_second_angstrom(self.test_flux, self.test_wavelength)
         np.testing.assert_allclose(result, expected_output, rtol=1e-6)
 
+
     def test_photon_cmsquared_second_angstrom2erg_cmsquared_second_angstrom(self):
         """Test conversion from photon/cm^2/s/Ångström to erg/cm^2/s/Ångström"""
         constant = 5.03411250E+07
@@ -96,12 +108,14 @@ class TestUnitsConversions(unittest.TestCase):
         result = units.photon_cmsquared_second_angstrom2erg_cmsquared_second_angstrom(self.test_flux, self.test_wavelength)
         np.testing.assert_allclose(result, expected_output, rtol=1e-6)
 
+
     def test_watt_metersquared_hertz2jansky(self):
         """Test conversion from W/m^2/Hz to Jansky"""
         constant = 1e26
         expected_output = self.test_flux * constant
         result = units.watt_metersquared_hertz2jansky(self.test_flux)
         np.testing.assert_allclose(result, expected_output, rtol=1e-6)
+
 
     def test_jansky2watt_metersquared_hertz(self):
         """Test conversion from Jansky to W/m^2/Hz"""
@@ -111,12 +125,14 @@ class TestUnitsConversions(unittest.TestCase):
         result = units.jansky2watt_metersquared_hertz(test_flux_jansky)
         np.testing.assert_allclose(result, expected_output, rtol=1e-6)
 
+
     def test_erg_cmsquared_second_hertz2jansky(self):
         """Test conversion from erg/cm^2/s/Hz to Jansky"""
         constant = 1e23
         expected_output = self.test_flux * constant
         result = units.erg_cmsquared_second_hertz2jansky(self.test_flux)
         np.testing.assert_allclose(result, expected_output, rtol=1e-6)
+
 
     def test_jansky2erg_cmsquared_second_hertz(self):
         """Test conversion from Jansky to erg/cm^2/s/Hz"""
@@ -126,12 +142,14 @@ class TestUnitsConversions(unittest.TestCase):
         result = units.jansky2erg_cmsquared_second_hertz(test_flux_jansky)
         np.testing.assert_allclose(result, expected_output, rtol=1e-6)
 
+
     def test_erg_cmsquared_second_angstrom2jansky(self):
         """Test conversion from erg/cm^2/s/Ångström to Jansky"""
         constant = 3.33564095e04
         expected_output = constant * self.test_flux * self.test_wavelength**2
         result = units.erg_cmsquared_second_angstrom2jansky(self.test_flux, self.test_wavelength)
         np.testing.assert_allclose(result, expected_output, rtol=1e-6)
+
 
     def test_jansky2erg_cmsquared_second_angstrom(self):
         """Test conversion from Jansky to erg/cm^2/s/Ångström"""
@@ -141,12 +159,14 @@ class TestUnitsConversions(unittest.TestCase):
         result = units.jansky2erg_cmsquared_second_angstrom(test_flux_jansky, self.test_wavelength)
         np.testing.assert_allclose(result, expected_output, rtol=1e-6)
 
+
     def test_watt_metersquared_micron2jansky(self):
         """Test conversion from W/m^2/μm to Jansky"""
         constant = 3.33564095e04
         expected_output = constant * self.test_flux * self.test_wavelength**2
         result = units.watt_metersquared_micron2jansky(self.test_flux, self.test_wavelength)
         np.testing.assert_allclose(result, expected_output, rtol=1e-6)
+
 
     def test_jansky2watt_metersquared_micron(self):
         """Test conversion from Jansky to W/m^2/μm"""
@@ -155,9 +175,9 @@ class TestUnitsConversions(unittest.TestCase):
         result = units.jansky2watt_metersquared_micron(self.test_flux, self.test_wavelength)
         np.testing.assert_allclose(result, expected_output, rtol=1e-6)
 
+
     def test_flambda2fnu(self):
         """Test conversion from Jansky to W/m^2/μm"""
-        fnu = units.flambda2fnu(self.test_flux*u.W/u.m**2/u.micron, self.test_wavelength*u.micron)
         expected_output = np.array([
             2.99792458e+14,
             2.87375019e+14,
@@ -172,6 +192,7 @@ class TestUnitsConversions(unittest.TestCase):
         )
         result = units.jansky2watt_metersquared_micron(self.test_flux, self.test_wavelength)
         np.testing.assert_allclose(result.value, expected_output, rtol=1e-6)
-        
+
+
 if __name__ == "__main__":
     unittest.main()
--- radiation_unit_conversion/units.py
+++ radiation_unit_conversion/units.py
@@ -1,4 +1,3 @@
-import numpy as np
 import astropy.constants as cst
 import astropy.units as u
 """
@@ -12,6 +11,7 @@ dependence require wavelength units rather than flux units, though conversion
 functions are also provided.
 """
 
+
 def fnu2flambda(input_flux, input_wavelength, output_units):
     """
     Converts flux from the f_nu (flux per unit frequency) form to the f_lambda 
@@ -21,10 +21,10 @@ def fnu2flambda(input_flux, input_wavelength, output_units):
     -----------
     input_flux : `astropy.units.Quantity`
         Flux in units of W/m^2/Hz or similar frequency-based units (e.g., erg/s/cm^2/Hz).
-    
+
     input_wavelength : `astropy.units.Quantity`
         Wavelength in units of meters, or similar wavelength-based units (e.g., microns, nanometers).
-    
+
     output_units : `astropy.units.Unit`
         The desired output units for the flux (e.g., W/m^2/micron, erg/s/cm^2/angstrom).
 
@@ -32,7 +32,7 @@ def fnu2flambda(input_flux, input_wavelength, output_units):
     --------
     `astropy.units.Quantity`
         The converted flux in the desired output units (e.g., W/m^2/micron, erg/s/cm^2/angstrom).
-    
+
     Notes:
     ------
     The conversion from f_nu to f_lambda uses the relationship:
@@ -59,6 +59,7 @@ def fnu2flambda(input_flux, input_wavelength, output_units):
 
     return output
 
+
 def flambda2fnu(input_flux, input_wavelength, output_units):
     """
     Converts flux from the f_lambda (flux per unit wavelength) form to the f_nu 
@@ -68,18 +69,17 @@ def flambda2fnu(input_flux, input_wavelength, output_units):
     -----------
     input_flux : `astropy.units.Quantity`
         Flux in units of W/m^2/micron or similar wavelength-based units (e.g., erg/s/cm^2/angstrom).
-    
+
     input_wavelength : `astropy.units.Quantity`
         Wavelength in units of meters, or similar wavelength-based units (e.g., microns, nanometers).
-    
+
     output_units : `astropy.units.Unit`
         The desired output units for the flux (e.g., W/m^2/Hz, erg/s/cm^2/Hz).
-
     Returns:
     --------
     `astropy.units.Quantity`
         The converted flux in the desired output units (e.g., W/m^2/Hz, erg/s/cm^2/Hz).
-    
+
     Notes:
     ------
     The conversion from f_lambda to f_nu uses the relationship:
@@ -106,141 +106,173 @@ def flambda2fnu(input_flux, input_wavelength, output_units):
 
     return output
 
+
 def wavelength2frequency(input_wavelength, output_units):
     input_wavelength.to(u.m)
-    return (cst.c/input_wavelength).to(output_units)
+    return (cst.c / input_wavelength).to(output_units)
+
 
 def frequency2wavelength(input_frequency, output_units):
     input_frequency.to(u.m)
-    return (cst.c/input_frequency).to(output_units)
+    return (cst.c / input_frequency).to(output_units)
+
 
 ## STScI direct conversion functions
 def watt_metersquared2erg_cmsquared_second(input_flux):
     # [Y erg/cm^2/s] = 1000 * [X W/m^2]
     return 1000 * input_flux
 
+
 def erg_cmsquared_second2watt_metersquared(input_flux):
-    return input_flux/1000
+    return input_flux / 1000
+
 
 def watt_metersquared_hertz2erg_cmsquared_second_hertz(input_flux):
     # [Y erg/cm^2/s/Hz] = 1000 * [X W/m^2/Hz]
     return 1000 * input_flux
 
-def erg_cmsquared_secondhertz2watt_metersquaredhertz(input_flux):
-    return input_flux/1000
 
-def watt_metersquared_hertz2erg_cmsquared_second_angstrom(input_flux,input_wavelength):
+def erg_cmsquared_secondhertz2watt_metersquaredhertz(input_flux):
+    return input_flux / 1000
+
+
+def watt_metersquared_hertz2erg_cmsquared_second_angstrom(input_flux, input_wavelength):
     # [Y erg/cm^2/s/A] = 2.99792458E+21 * [X1 W/m^2/Hz] / [X2 A]^2
     constant = 2.99792458e21
     return constant * input_flux / input_wavelength**2
 
-def erg_cmsquared_second_angstrom2watt_metersquared_hertz(input_flux,input_wavelength):
+
+def erg_cmsquared_second_angstrom2watt_metersquared_hertz(input_flux, input_wavelength):
     constant = 2.99792458e21
     return  input_flux * input_wavelength**2 / constant
 
-def watt_metersquared_micron2watt_metersquared_hertz(input_flux,input_wavelength):
+
+def watt_metersquared_micron2watt_metersquared_hertz(input_flux, input_wavelength):
     # [Y W/m^2/um] = 2.99792458E+14 * [X1 W/m^2/Hz] / [X2 um]^2
     constant = 2.99792458e14
-    return constant * input_flux/input_wavelength**2
+    return constant * input_flux / input_wavelength**2
 
-def watt_metersquared_hertz2watt_metersquared_micron(input_flux,input_wavelength):
+
+def watt_metersquared_hertz2watt_metersquared_micron(input_flux, input_wavelength):
     constant = 2.99792458e14
     return input_flux*input_wavelength**2 / constant
 
-def erg_cmsquared_hertz2photon_cmsquared_second_micron(input_flux,input_wavelength):
+
+def erg_cmsquared_hertz2photon_cmsquared_second_micron(input_flux, input_wavelength):
     # [Y photon/cm^2/s/um] = 1.50918896E+22 * [X1 erg/cm^2/Hz] / [X2 um]
     constant = 1.50918896e22
     return constant * input_flux/input_wavelength
 
-def photon_cmsquared_second_micron2erg_cmsquared_hertz(input_flux,input_wavelength):
+
+def photon_cmsquared_second_micron2erg_cmsquared_hertz(input_flux, input_wavelength):
     constant = 1.50918896e22
-    return input_flux*input_wavelength/constant 
+    return input_flux * input_wavelength / constant 
+
 
 def watt_metersquared_micron2photon_cmsquared_second_micron(input_flux,input_wavelength):
     # [Y photon/cm^2/s/um] = 5.03411250E+14 * [X1 W/m^2/um] * [X2 um]
     constant = 5.03411250e14
     return constant * input_flux * input_wavelength
 
-def photon_cmsquared_second_micron2watt_metersquared_micron(input_flux,input_wavelength):
+
+def photon_cmsquared_second_micron2watt_metersquared_micron(input_flux, input_wavelength):
     constant = 5.03411250e14
     return input_flux / input_wavelength / constant
 
-def erg_cmsquared_second_angstrom2photon_cmsquared_second_angstrom(input_flux,input_wavelength):
+
+def erg_cmsquared_second_angstrom2photon_cmsquared_second_angstrom(input_flux, input_wavelength):
     # [Y photon/cm^2/s/A] = 5.03411250E+07 * [X1 erg/cm^2/s/A] * [X2 A]
     constant = 5.03411250E+07 
     return constant * input_flux * input_wavelength
 
-def photon_cmsquared_second_angstrom2erg_cmsquared_second_angstrom(input_flux,input_wavelength):
+
+def photon_cmsquared_second_angstrom2erg_cmsquared_second_angstrom(input_flux, input_wavelength):
     constant = 5.03411250E+07 
     return input_flux / input_wavelength / constant
+
 
 def watt_metersquared_hertz2jansky(input_flux):
     # [Y Jy] = 1.0E+26 * [X W/m^2/Hz]
     constant = 1e26
     return constant * input_flux
 
+
 def jansky2watt_metersquared_hertz(input_flux):
     constant = 1e26
     return input_flux / constant
+
 
 def erg_cmsquared_second_hertz2jansky(input_flux):
     # [Y Jy] = 1.0E+23 * [X erg/cm^2/s/Hz]
     constant = 1e23
     return input_flux * constant
 
+
 def jansky2erg_cmsquared_second_hertz(input_flux):
     constant = 1e23
     return input_flux / constant
 
-def erg_cmsquared_second_angstrom2jansky(input_flux,input_wavelength):
+
+def erg_cmsquared_second_angstrom2jansky(input_flux, input_wavelength):
     # [Y Jy] = 3.33564095E+04 * [X1 erg/cm^2/s/A] * [X2 A]^2
     constant = 3.33564095e04 
     return constant * input_flux * input_wavelength**2
 
-def jansky2erg_cmsquared_second_angstrom(input_flux,input_wavelength):
+
+def jansky2erg_cmsquared_second_angstrom(input_flux, input_wavelength):
     constant = 3.33564095e04 
     return input_flux / input_wavelength**2 / constant
 
-def watt_metersquared_micron2jansky(input_flux,input_wavelength):
+
+def watt_metersquared_micron2jansky(input_flux, input_wavelength):
     # [Y erg/cm^2/s/A] = 2.99792458E-05 * [X1 Jy] / [X2 A]^2
     constant = 3.33564095e04 
     return constant * input_flux * input_wavelength**2
 
-def jansky2watt_metersquared_micron(input_flux,input_wavelength):
-    fnu = watt_metersquared_micron2watt_metersquared_hertz(input_flux,input_wavelength)
+
+def jansky2watt_metersquared_micron(input_flux, input_wavelength):
+    fnu = watt_metersquared_micron2watt_metersquared_hertz(input_flux, input_wavelength)
     return watt_metersquared_hertz2jansky(fnu)
 
-def jansky2photon_cmsquared_second_angstrom(input_flux,input_wavelength):
+
+def jansky2photon_cmsquared_second_angstrom(input_flux, input_wavelength):
     # [Y photon/cm^2/s/A] = 1.50918896E+03 * [X1 Jy] / [X2 A]
     constant = 1.50918896e03
     return constant * input_flux / input_wavelength
 
-def photon_cmsquared_second_angstrom2jansky(input_flux,input_wavelength):
+
+def photon_cmsquared_second_angstrom2jansky(input_flux, input_wavelength):
     constant = 1.50918896e03
     return input_flux * input_wavelength  / constant
+
 
 def rayleigh2photon_cmsquared_second_angstrom_steradian(input_flux):
     # [Y photon/cm^2/s/A/sr] = 7.9577539E+04 [X Rayleigh]
     constant = 7.9577539e04 
     return constant * input_flux
 
+
 def photon_cmsquared_second_angstrom_steradian2rayleigh(input_flux):
     constant = 7.9577539e04 
     return  input_flux / constant
+
 
 def rayleigh2photon_cmsquared_angstrom_degreesquared(input_flux):
     # [Y photon/cm^2/s/A/deg^2] = 2.4240705E+01 [X Rayleigh]
     constant = 2.4240705e01
     return constant * input_flux
 
+
 def photon_cmsquared_angstrom_degreesquared2rayleigh(input_flux):
     constant = 2.4240705e01
     return  input_flux / constant
+
 
 def rayleigh2photon_cmsquared_second_angstrom_arcsecondsquared(input_flux):
     # [Y photon/cm^2/s/A/arcsec^2] = 1.8704247E-06 [X Rayleigh]
     constant = 1.8704247e06 
     return constant * input_flux
+
 
 def photon_cmsquared_second_angstrom_arcsecondsquared2rayleigh(input_flux):
     constant = 1.8704247e06 
